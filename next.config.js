fe63b95b50fe680f6da93adfef201566db35d90e
kensuke0518 <k.com.play@gmail.com>
2021-01-30 18:55:46 +0900

    修正

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     exportPathMap: function () {
         return {
             '/': { page: '/' },
-            '/other': { page: '/other' }
+            '/other': { page: '/other.js' }
         }
     },
     assetPrefix: process.env.NODE_ENV === "production" ? "/project_name" : ""
